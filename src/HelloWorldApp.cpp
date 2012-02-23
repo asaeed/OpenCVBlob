5d5a8d9ed44b3e836e743fb63b46711acb19ca0b
Ahmad Saeed <asaeed@gmail.com>
2012-02-23 02:48:12 -0500

    basic cv

--- src/HelloWorldApp.cpp
+++ src/HelloWorldApp.cpp
@@ -11,9 +11,12 @@
 #include "cinder/gl/Texture.h"
 #include "Resources.h"
 #include "gl.h"
+#include <vector>
 
 #include "cinder/Capture.h"
 #include "CinderOpenCV.h"
+
+using namespace std;
 
 class HelloWorldApp : public ci::app::AppBasic {
 public:
@@ -37,17 +40,38 @@ void HelloWorldApp::setup() {
 
 void HelloWorldApp::update() {
 	if( mCap && mCap.checkNewFrame() ) {
-		cv::Mat input( toOcv( mCap.getSurface() ) ), output;
+		cv::Mat input( toOcv( mCap.getSurface() ) ), flipped, grayscale, blurred, thresholded, output;
+
+		cv::flip(input, flipped, 1);
+		cv::flip(input, output, 1);
+		cv::cvtColor(flipped, grayscale, CV_RGB2GRAY);
+		cv::blur(grayscale, blurred, cv::Size(10,10));
+		cv::threshold( blurred, thresholded, 75.0f, 255, CV_THRESH_BINARY);
+
+		// findContours requires grayscale image.  also it modifies input image so it won't be usable
+		std::vector<std::vector<cv::Point> > contours;
+		cv::findContours(thresholded, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+		for (vector<vector<cv::Point> >::iterator it=contours.begin() ; it < contours.end(); it++ ){
+			cv::Point2f center;
+			float radius;
+			vector<cv::Point> pts = *it;
+			cv::Mat pointsMatrix = cv::Mat(pts);
+			cv::minEnclosingCircle(pointsMatrix, center, radius);
+			cv::Scalar color( 0, 255, 0 );
+			cv::circle(output, center, radius, color);
+		}
+
 
 	//	cv::threshold( input, output, 128, 255, CV_8U );
 
-		cv::Sobel( input, output, CV_8U, 1, 0 );
+	//	cv::Sobel( input, output, CV_8U, 1, 0 );
 
 	//	cv::Laplacian( input, output, CV_8U );
 
-	//	cv::circle( output, toOcv( Vec2f(200, 200) ), 300, toOcv( Color( 0, 0.5f, 1 ) ), -1 );
+	//	cv::circle( output, toOcv( ci::Vec2f(200, 200) ), 300, toOcv( ci::Color( 0, 0.5f, 1 ) ), -1 );
 
-	//	cv::line( output, cv::Point( 1, 1 ), cv::Point( 30, 30 ), toOcv( Color( 1, 0.5f, 0 ) ) );
+	//	cv::line( output, cv::Point( 1, 1 ), cv::Point( 30, 30 ), toOcv( ci::Color( 1, 0.5f, 0 ) ) );
 
 		mTexture = ci::gl::Texture( ci::fromOcv( output ) );
 	}
